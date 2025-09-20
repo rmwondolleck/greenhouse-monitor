b2ac30755e9cb822bff9ecac80fdde88ec643c31
Ryan Wondolleck <ryan.wondolleck@gmail.com>
2025-09-20 09:31:42 -0700

    working build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 7ae74a6 (fix history)
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -28,18 +24,4 @@ export default defineConfig({
             '@': path.resolve(__dirname, './src'),
         },
     },
-<<<<<<< HEAD
 });
-=======
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-});
->>>>>>> 27c2c41 (Initial commit from create-react-router)
-=======
-});
->>>>>>> 7ae74a6 (fix history)
